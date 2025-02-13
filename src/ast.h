5f0130a4fed7ea58730aee3fe4980b3e7570c487
VHHV <vhhv@VHHVs-MacBook-Pro.local>
2025-02-13 14:06:30 +0700

    test

--- src/ast.h
+++ src/ast.h
@@ -1,9 +1,6 @@
 #ifndef AST_H
 #define AST_H
 
-//========================
-//  AST NODE TYPES ENUM
-//========================
 typedef enum {
   INT_EXP,
   NULL_EXP,
@@ -24,9 +21,6 @@ typedef enum {
   EXP_STMT
 } AstTag;
 
-//========================
-//  BASE STRUCTURES
-//========================
 typedef struct {
   AstTag tag;
 } Exp;
@@ -39,9 +33,9 @@ typedef struct {
   AstTag tag;
 } ScopeStmt;
 
-//========================
-//  EXPRESSION NODES
-//========================
+
+
+
 typedef struct {
   AstTag tag;
   int value;
@@ -119,9 +113,7 @@ typedef struct {
   char* name;
 } RefExp;
 
-//========================
-//  SLOT STATEMENTS
-//========================
+//Slot Statements
 typedef struct {
   AstTag tag;  
   char* name;
@@ -136,9 +128,7 @@ typedef struct {
   ScopeStmt* body;
 } SlotMethod;
 
-//========================
-//  SCOPE STATEMENTS
-//========================
+//Scope Statements
 typedef struct {
   AstTag tag;
   char* name;
@@ -164,44 +154,13 @@ typedef struct {
   Exp* exp;
 } ScopeExp;
 
-//========================
-//  FUNCTION DECLARATIONS
-//========================
+void print_exp (Exp* e);
+void print_slotstmt (SlotStmt* s);
+void print_scopestmt (ScopeStmt* s);
 
-// AST Constructors
-Exp* make_IntExp(int value);
-Exp* make_NullExp();
-Exp* make_PrintfExp(char* format, int nexps, Exp** exps);
-Exp* make_ArrayExp(Exp* length, Exp* init);
-Exp* make_ObjectExp(Exp* parent, int nslots, SlotStmt** slots);
-Exp* make_SlotExp(char* name, Exp* exp);
-Exp* make_SetSlotExp(char* name, Exp* exp, Exp* value);
-Exp* make_CallSlotExp(char* name, Exp* exp, int nargs, Exp** args);
-Exp* make_CallExp(char* name, int nargs, Exp** args);
-Exp* make_SetExp(char* name, Exp* exp);
-Exp* make_IfExp(Exp* pred, ScopeStmt* conseq, ScopeStmt* alt);
-Exp* make_WhileExp(Exp* pred, ScopeStmt* body);
-Exp* make_RefExp(char* name);
-
-SlotStmt* make_SlotVar(char* name, Exp* exp);
-SlotStmt* make_SlotMethod(char* name, int nargs, char** args, ScopeStmt* body);
-
-ScopeStmt* make_ScopeVar(char* name, Exp* exp);
-ScopeStmt* make_ScopeFn(char* name, int nargs, char** args, ScopeStmt* body);
-ScopeStmt* make_ScopeSeq(ScopeStmt* a, ScopeStmt* b);
-ScopeStmt* make_ScopeExp(Exp* exp);
-
-// AST Printing
-void print_exp(Exp* e);
-void print_slotstmt(SlotStmt* s);
-void print_scopestmt(ScopeStmt* s);
-void print_tree(ScopeStmt* stmt, int depth);
-void print_exp_tree(Exp* e, int depth);
-
-// AST Loading (Deserialization)
-Exp* read_exp();
-SlotStmt* read_slot();
-ScopeStmt* read_scopestmt();
-ScopeStmt* read_ast(char* filename);
+Exp* read_exp ();
+SlotStmt* read_slot ();
+ScopeStmt* read_scopestmt ();
+ScopeStmt* read_ast (char* filename);
 
 #endif
